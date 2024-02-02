71aa6acbde19b2c7d3c9bcaa07a86865bd8a372e
Aya Saeed Mostafa Mohammed <ayasmostafa257@gmail.com>
2024-02-02 06:29:53 +0200

    IntialCreateMigration

--- Back-end-StockExchange/StockExchange/obj/Debug/net8.0/StockExchange.AssemblyInfo.cs
+++ Back-end-StockExchange/StockExchange/obj/Debug/net8.0/StockExchange.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockExchange")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58034f9a2698f0cf275e2580a95023f8212ad6ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619344c71efb73b1a88fe139ef454bc6458fcfb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockExchange")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockExchange")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
