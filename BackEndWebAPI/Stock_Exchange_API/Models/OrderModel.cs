7ff131bce30cee8ac4cd88ac228e777f2fc84557
Aya Saeed Mostafa Mohammed <ayasmostafa257@gmail.com>
2024-02-02 05:47:52 +0200

    intial

---
+++ BackEndWebAPI/Stock_Exchange_API/Models/OrderModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Stock_Exchange_API.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int StockSymbol { get; set; }
+        public string OrderType { get; set; } // 'buy' or 'sell'
+        public int Quantity { get; set; }
+        // Navigation property
+        public StockModel Stock { get; set; }
+    }
+}
---
+++ BackEndWebAPI/Stock_Exchange_API/Models/StockModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stock_Exchange_API.Models
+{
+    public class StockModel
+    {
+        [Key]
+        public int Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        // Configure one-to-many relationship between StockModel and OrderModel
+        public ICollection<OrderModel> Orders { get; set; }
+    }
+}
